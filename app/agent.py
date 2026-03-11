56d407092839ae0f889aaa5d4cd42186400df55f
rikulpatel@gmail.com <rikulpatel@gmail.com>
2026-03-10 19:36:53 -0500

    codecrafters submit [skip ci]

--- app/agent.py
+++ app/agent.py
@@ -12,11 +12,12 @@ class Agent:
 
     def start_loop(self, message: str):
         
+        messages = [{"role": "user", "content": message}]
         while True:
 
             chat = self.client.chat.completions.create(
                 model="anthropic/claude-haiku-4.5",
-                messages=[{"role": "user", "content": message}],
+                messages=messages,
                 tools=[read_file_tool_spec]
             )
 
@@ -24,21 +25,26 @@ class Agent:
                 raise RuntimeError("no choices in response")
 
             print(f"chat.choices: {chat.choices}", file=sys.stderr)
-            choice = chat.choices[0]
 
-            if choice.message.tool_calls is not None:
+            assistant_message = chat.choices[0].message
+            messages.append(assistant_message)
 
-                for tool_call in choice.message.tool_calls:
+            if assistant_message.tool_calls is not None:
+                print(f"tool_calls: {assistant_message.tool_calls}", file=sys.stderr)
+
+                for tool_call in assistant_message.tool_calls:
 
                     tool_name = tool_call.function.name
                     tool_args = json.loads(tool_call.function.arguments)
 
-                    result = {}
-                    result["role"] = "tool"
-                    result["tool_call_id"] = tool_call.id
-                    result["content"] = run_tool(tool_name=tool_name, tool_args=tool_args)
+                    result = run_tool(tool_name=tool_name, tool_args=tool_args)
+                    messages.append({
+                        "role": "tool", 
+                        "tool_call_id": tool_call.id, 
+                        "name": tool_name, 
+                        "content": result
+                    })                  
 
-                    message += json.dumps(result)
             else:
                 print(chat.choices[0].message.content)
                 break
