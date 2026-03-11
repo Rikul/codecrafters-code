33314695b22afffff3e1d2255e60fdb7f3b4cd2a
rikulpatel@gmail.com <rikulpatel@gmail.com>
2026-03-10 19:16:10 -0500

    codecrafters submit [skip ci]

--- app/agent.py
+++ app/agent.py
@@ -10,34 +10,36 @@ class Agent:
     def __init__(self):
         self.client = Client().get_client()
 
-    def start_loop(self, prompt: str):
+    def start_loop(self, message: str):
         
-        chat = self.client.chat.completions.create(
-            model="anthropic/claude-haiku-4.5",
-            messages=[{"role": "user", "content": prompt}],
-            tools=[read_file_tool_spec]
-        )
+        while True:
 
-        if not chat.choices or len(chat.choices) == 0:
-            raise RuntimeError("no choices in response")
+            chat = self.client.chat.completions.create(
+                model="anthropic/claude-haiku-4.5",
+                messages=[{"role": "user", "content": message}],
+                tools=[read_file_tool_spec]
+            )
 
-        # You can use print statements as follows for debugging, they'll be visible when running tests.
-        print(f"chat.choices: {chat.choices}", file=sys.stderr)
+            if not chat.choices or len(chat.choices) == 0:
+                raise RuntimeError("no choices in response")
 
-        for choice in chat.choices:
+            print(f"chat.choices: {chat.choices}", file=sys.stderr)
+            choice = chat.choices[0]
 
             if choice.message.tool_calls is not None:
-            #if choice.message.role == "assistant" and len(choice.message.tool_calls) > 0:
 
                 for tool_call in choice.message.tool_calls:
-                    #print(f"tool_call: {tool_call} tool_name: {tool_call.function.name} ", file=sys.stderr)
 
                     tool_name = tool_call.function.name
                     tool_args = json.loads(tool_call.function.arguments)
 
-                    result = run_tool(tool_name=tool_name, tool_args=tool_args)
-                    print(result)
+                    result = {}
+                    result["role"] = "tool"
+                    result["tool_call_id"] = tool_call.id
+                    result["content"] = run_tool(tool_name=tool_name, tool_args=tool_args)
 
+                    message += json.dumps(result)
             else:
                 print(chat.choices[0].message.content)
+                break
 
